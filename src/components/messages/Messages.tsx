f34802a24a4a59f69811c72e6070571298c2148a
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-16 00:09:46 +0400

    test react-infinite-scroller

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -2,6 +2,7 @@ import {FC, useState, useEffect} from 'react';
 import styles from './Messages.module.scss';
 import axios from 'axios';
 import MessageEl, {type Message} from './components/MessageEl';
+import InfiniteScroll from 'react-infinite-scroller';
 
 interface MessagesProps {
   id: number;
@@ -9,38 +10,67 @@ interface MessagesProps {
 
 const Messages: FC<MessagesProps> = ({id}) => {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [scrollParentRef, setScrollParentRef] = useState<HTMLDivElement | null>(null);
+
+  const fetchMessageChunk = async () => {
+    if (isLoading) return;
+    setLoading(true);
+
+    console.log("fetchMessageChunk");
+    try {
+      const oldesMessageId = messages.length > 0 ? messages[messages.length - 1].id : 0;
+      
+      const response = await axios.get(
+        `/api/chats/${id}/messages?fromMessageId=${oldesMessageId}`
+      );
+
+      const newMessages = response.data;
+      setMessages([...newMessages, ...messages]);
+      setHasMore(newMessages.length > 0);
+
+      // TODO on load last message chunk
+    } catch (error) {
+      console.error("Error on receive data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchChats = async () => {
-      try {
-        const response = await axios.get(`/api/chats/${id}/messages`);
-        setMessages(response.data);
-      } catch (error) {
-        console.error("Error on receive data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchChats();
-
     return () => {
       setMessages([]);
-      setLoading(true);
+      setLoading(false);
+      setHasMore(true);
     }
+  }, []);
 
-  }, [id]);
+  const messagesRendered = messages.map((message) => {
+    return <MessageEl message={message} key={message.id} isOwnMessage={message.sender !== id}/>
+  });
 
-  return <div className={styles.layout}>
-    {loading
+  const InfiniteScrollEl = <InfiniteScroll
+      pageStart={0}
+      loadMore={fetchMessageChunk}
+      hasMore={hasMore}
+      isReverse={true}
+      initialLoad={true}
+      // useWindow={false}
+      // getScrollParent={() => scrollParentRef}
+      // loader={<div className={styles.loader} key={0}>Loading ...</div>}
+  >
+      {messagesRendered}
+  </InfiniteScroll>
+
+  return <div className={styles.layout} ref={(ref) => setScrollParentRef(ref)}>
+    {InfiniteScrollEl}
+    {/* {loading
       ? <div className={styles.empty}>Loading...</div>
       : messages.length === 0
         ? <div className={styles.empty}>Messages not loaded to server</div>
-        : messages.map((message) => {
-        return <MessageEl message={message} key={message.id} isOwnMessage={message.sender !== id}/>
-      })
-    }
+        : InfiniteScrollEl
+    } */}
   </div>
 };
 
