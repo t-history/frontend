d181879a8eeef06ea9cd8f24f1f6f7e411257646
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-15 21:33:58 +0400

    firs bad solution

--- src/components/chats/Chats.tsx
+++ src/components/chats/Chats.tsx
@@ -1,18 +1,48 @@
 import {FC} from 'react';
-import Chat from './components/Chat';
-import { TChat } from './components/Chat'
+import ChatItem from './components/ChatItem';
+import { TChat } from './components/ChatItem'
 import styles from './Chats.module.scss';
+
+import dynamic from 'next/dynamic';
+import { FixedSizeList as List } from 'react-window';
+
+const NoSSRList = dynamic(() => Promise.resolve(List), { ssr: false });
 
 interface ChatsProps {
   chats: TChat[];
 }
 
-const Chats: FC<ChatsProps> = ({chats}) => {
+type ListItemProps = {
+  index?: number;
+  style?: React.CSSProperties;
+};
+
+const ChatList: FC<ChatsProps> = ({chats}) => {
+  const ListComponent = typeof window === 'undefined' ? List : NoSSRList;
+  const preRenderCount = 100;
+  const itemSize = 72;
+
+  const ListItem: React.FC<ListItemProps> = ({ index = 0, style }) => {
+    const chat = chats[index + preRenderCount];
+    return <div style={style}>
+        <ChatItem chat={chat} key={index + preRenderCount} />;
+    </div>
+  };
+
   return <div className={styles.layout}>
-    {chats.map((chat, i) =>
-      <Chat chat={chat} key={i}/>
-    )}
+    {Array.from({ length: preRenderCount }).map((_, i) => (
+      <ListItem />
+    ))}
+    <ListComponent
+      height={chats.length * itemSize}
+      itemCount={chats.length - preRenderCount}
+      itemSize={itemSize}
+      width='100%'
+      initialScrollOffset={preRenderCount * itemSize}
+    >
+      <ListItem />
+    </ListComponent>
   </div>
 };
 
-export default Chats;
+export default ChatList;
--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -1,7 +1,8 @@
-import {FC, useState, useEffect} from 'react';
+import {FC, useState, useEffect, useRef} from 'react';
 import styles from './Messages.module.scss';
 import axios from 'axios';
 import MessageEl, {type Message} from './components/MessageEl';
+import { useInView } from 'react-intersection-observer';
 
 interface MessagesProps {
   id: number;
@@ -10,14 +11,33 @@ interface MessagesProps {
 const Messages: FC<MessagesProps> = ({id}) => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [oldestMessageId, setOldestMessageId] = useState<Number>(0);
+
+  // const fetchChats = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `/api/chats/${id}/messages?fromMessageId=${oldestMessageId}&limit=20`
+  //     );
+  //     const messageChunk = response.data;
+  //     setMessages([...messages, ...messageChunk]);
+  //     setOldestMessageId(messageChunk[messageChunk.length - 1].id);
+  //   } catch (error) {
+  //     console.error("Error on fetch message chunk:", error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   useEffect(() => {
     const fetchChats = async () => {
       try {
-        const response = await axios.get(`/api/chats/${id}/messages`);
-        setMessages(response.data);
+        const response = await axios.get(
+          `/api/chats/${id}/messages`
+        );
+        const messageChunk = response.data;
+        setMessages([...messages, ...messageChunk]);
       } catch (error) {
-        console.error("Error on receive data:", error);
+        console.error("Error on fetch message chunk:", error);
       } finally {
         setLoading(false);
       }
@@ -32,14 +52,31 @@ const Messages: FC<MessagesProps> = ({id}) => {
 
   }, [id]);
 
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      console.log('In view');
+    }
+  }, [inView]);
+
   return <div className={styles.layout}>
     {loading
       ? <div className={styles.empty}>Loading...</div>
       : messages.length === 0
         ? <div className={styles.empty}>Messages not loaded to server</div>
-        : messages.map((message) => {
-        return <MessageEl message={message} key={message.id} isOwnMessage={message.sender !== id}/>
-      })
+        : messages.map((message, i) => {
+            if (i === 15) message.content += ' alalala'
+            return <MessageEl
+              ref={i === 15 ? ref : null}
+              message={message}
+              key={message.id}
+              isOwnMessage={message.sender !== id}
+            />
+          })
     }
   </div>
 };
