95ee1b23b33f4eefd7bb05ebcfb463805c554b58
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-25 18:26:24 +0400

    Added visualization for queue

--- src/components/chats/Header.tsx
+++ src/components/chats/Header.tsx
@@ -1,8 +1,35 @@
-import {FC} from 'react';
+import {FC, useEffect, useState} from 'react';
 import styles from './Header.module.scss';
 
+interface IData {
+  wait: number;
+  completed?: number;
+  failed?: number;  
+}
+
 const Header: FC = () => {
-  return <div className={styles.layout}>Ts</div>
+  const [data, setData] = useState<IData | null>(null);
+
+  useEffect(() => {
+    const source = new EventSource('/api/queue/sse');
+
+    source.onmessage = (event) => {
+      console.log('sse');
+      const newData = JSON.parse(event.data);
+      setData(newData);
+    };
+    return () => {
+      source.close();
+    };
+  }, []);    
+
+  return <div className={styles.layout}>
+    {data &&
+      <div className={styles.wait}>
+        {data.wait} - {data.completed} - {data.failed}
+      </div>
+    }
+  </div>
 };
 
 export default Header;
