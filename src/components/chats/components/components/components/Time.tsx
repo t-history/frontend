2a392dab5e6145ad550661d405073c3c9366dc70
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-26 19:05:28 +0400

    Fixed time display for last message in chat list

--- src/components/chats/components/components/components/Time.tsx
+++ src/components/chats/components/components/components/Time.tsx
@@ -6,17 +6,46 @@ interface TimeProps {
   unixtime: number;
 }
 
-// if today then time hh:mm, if this week then day (like Mon, Tue ... Sun), else date in format dd.mm.yy
+const isToday = (date: Date) => {
+  const now = new Date();
+  return date.getDate() === now.getDate() &&
+    date.getMonth() === now.getMonth() &&
+    date.getFullYear() === now.getFullYear();
+}
+
+function isWithinWeekToTomorrow(dateToCheck: Date): boolean {
+  const date = new Date(dateToCheck);
+  const today = new Date();
+  const weekBeforeTomorrow = new Date();
+  weekBeforeTomorrow.setDate(today.getDate() - 6);
+
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  tomorrow.setHours(0, 0, 0, 0);
+  weekBeforeTomorrow.setHours(0, 0, 0, 0);
+  date.setHours(0, 0, 0, 0);
+
+  return date >= weekBeforeTomorrow && date < tomorrow;
+};
+
+function isLastYear(date: Date): boolean {
+  const today: Date = new Date();
+  const lastYear: Date = new Date(today.getFullYear() - 1, today.getMonth()+1);
+  
+  return date >= lastYear && date < today;
+}
+
 const Time: FC<TimeProps> = ({ unixtime }) => {
   const date = new Date(unixtime * 1000);
-  const now = new Date();
-  const diff = now.getTime() - date.getTime();
-  const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+
+  const dateRange = isToday(date) ? 'today' : isWithinWeekToTomorrow(date) ? 'week' : isLastYear(date) ? 'year' : 'old';
 
   return <span className={styles.layout}>
-    {diffDays === 0 && date.toLocaleTimeString()}
-    {diffDays > 0 && diffDays < 7 && date.toLocaleDateString('en-US', { weekday: 'short' })}
-    {diffDays >= 7 && date.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' })}
+    {dateRange === 'today' && date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+    {dateRange === 'week' && date.toLocaleDateString('en-US', { weekday: 'short' })}
+    {dateRange === 'year' && date.toLocaleDateString('en-US', { month: 'short', day: '2-digit' })}
+    {dateRange === 'old' && date.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' })}
   </span>
 }
 
