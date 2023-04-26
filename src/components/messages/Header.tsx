fa59c1627127407d50b21c8927c97d081b528ecc
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-26 15:41:12 +0400

    Disabled sync button & added spin for loading chat

--- src/components/messages/Header.tsx
+++ src/components/messages/Header.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { FC } from 'react';
-import { FaSyncAlt } from 'react-icons/fa';
+import { VscSync } from 'react-icons/vsc';
 
 import Avatar from '@/components/ui/Avatar';
 import { type IChat } from '@/interfaces/Chat';
@@ -16,16 +16,28 @@ const Header: FC<HeaderProps> = ({ chat }) => {
   if (!chat) {
     return <div className={styles.layout}>No chat</div>
   }
+  const isChatIdle = chat.status === 'idle';
 
   const handleSync = () => {
+    if (!isChatIdle) return;
     axios.post(`/api/queue/chat/${chat.id}/sync`)
   };
+
 
   return <div className={styles.layout}>
     <Avatar title={chat.title} status={chat.status} />
     {chat && chat.title}
     <div className={styles.actions}>
-      <button className={styles.actions__item} onClick={handleSync}><FaSyncAlt /></button>
+      <button
+        className={`
+          ${styles.actions__item}
+          ${!isChatIdle && styles.actions__item__disabled}
+          
+        `}
+        onClick={handleSync}
+      >
+        <VscSync className={`${!isChatIdle && styles.actions__item__spin}`} />
+      </button>
     </div>
   </div>
 };
